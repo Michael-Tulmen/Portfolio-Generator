82d84d4eabcddbc3e65a032dfa90e52d4c543dc6
Michael Tulmen <michaeltulmen@gmail.com>
2022-04-23 22:11:01 -0500

    node project can now generate an html file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if(err) throw err;
 
-  console.log('================');
+  console.log('Portfolio Complete! Check out index.html to see the output');
+});
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
